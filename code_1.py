0a5a11a5af4ca9b6412ff5b767cfd9bca504a50c
Lobo045 <lopezabraham@outlook.com>
2024-02-04 22:52:47 -0600

    4th commit

--- code_1.py
+++ code_1.py
@@ -16,12 +16,12 @@ class ClientManager:
         if not os.path.exists(client_file_path):
             with open(client_file_path, 'w') as client_file:
                 client_file.write(f"Client Name: {client_name}\nDescription: ")
-            print(f"The client '{client_name}' has been created.")
+            print(f"El cliente '{client_name}' ha sido creado.")
 
     def edit_client(self):
-        client_name = input("Enter client name to edit: ")
-        new_name = input("Enter new client name (press Enter to keep the same): ") or client_name
-        new_description = input("Enter new description (press Enter to keep the same): ")
+        client_name = input("Cual cliente desa editar: ")
+        new_name = input("Ingrese el nombre nuevo del cliente (ENTER si desea dejarlo igual): ") or client_name
+        new_description = input("Ingrese nueva descripcion (ENTER si desea dejarla igual): ")
 
         client_file_path = os.path.join(self.data_folder, f'{client_name}.txt')
 
@@ -42,25 +42,25 @@ class ClientManager:
             if new_name != client_name:
                 new_client_file_path = os.path.join(self.data_folder, f'{new_name}.txt')
                 os.rename(client_file_path, new_client_file_path)
-                print(f"The client '{client_name}' has been edited and renamed to '{new_name}'.")
+                print(f"El cliente '{client_name}' ha sido renombrado a '{new_name}'.")
             else:
-                print(f"The client '{client_name}' has been edited.")
+                print(f"El cliente '{client_name}' ha sido editado.")
 
         else:
-            print(f"The client '{client_name}' does not exist.")
+            print(f"El cliente '{client_name}' no existe.")
 
     def delete_client(self):
-        client_name = input("Enter client name to delete: ")
+        client_name = input("Ingrese el nombre del cliente que desea borrar: ")
         client_file_path = os.path.join(self.data_folder, f'{client_name}.txt')
 
         if os.path.exists(client_file_path):
             os.remove(client_file_path)
-            print(f"The client '{client_name}' has been deleted.")
+            print(f"El cliente '{client_name}' ha sido borrado.")
         else:
-            print(f"The client '{client_name}' does not exist.")
+            print(f"El cliente '{client_name}' no existe.")
 
     def list_clients(self):
-        print("List of clients:")
+        print("Lista de clientes:")
         for file_name in os.listdir(self.data_folder):
             if file_name.endswith(".txt"):
                 print(file_name[:-4]) 
@@ -70,13 +70,13 @@ if __name__ == "__main__":
 
     while True:
         print("\nOptions:")
-        print("1. Create clients")
-        print("2. Edit clients")
-        print("3. Delete clients")
-        print("4. List clients")
-        print("5. Exit")
+        print("1. Crear clientes")
+        print("2. Editar clientes")
+        print("3. Borrar clientes")
+        print("4. Lita de clientes")
+        print("5. Salir")
 
-        choice = input("Choose an option (1-5): ")
+        choice = input("Elija una opcion (1-5): ")
 
         if choice == '1':
             manager.create_client()
@@ -90,4 +90,4 @@ if __name__ == "__main__":
             print("Exiting the application.")
             break
         else:
-            print("Please choose a number between 1 and 5.")
+            print("Favor de elegir una opcion entre el 1 y el 5.")
